b7a9ac4c2ebaf1e9f7b0f948ad80b996610d01bd
jeremdh62 <jerem.dha@gmail.com>
2020-06-25 17:46:46 +0200

    test_persistance

--- src/components/PharmacieMap.vue
+++ src/components/PharmacieMap.vue
@@ -1,20 +1,21 @@
 <template>
   <div> 
-    <div v-for="laPharmacie in pharmacie" :key="pharmacie.indexOf(laPharmacie)">
+    <p> laPharmacie : {{ pharmacie }} </p>
+   <!-- <div v-for="laPharmacie in pharmacie" :key="pharmacie.indexOf(laPharmacie)">
     <p> nom : {{ laPharmacie.nom}} </p>
        </div>
 
-<GmapMap :center="{lat:50.485719,lng:2.864796}" :zoom="10" map-type-id="terrain" style="width: 100%; height: 550px" >
+<GmapMap :center="{lat:50.485719,lng:2.864796}" :zoom="9" map-type-id="terrain" style="width: 100%; height: 550px" >
 <div v-for="laPharmacie in pharmacie" :key="pharmacie.indexOf(laPharmacie)">
   <gmap-marker :position="{lat: laPharmacie.latlng.Rc ,lng: laPharmacie.latlng.Ac }">
 
-  </gmap-marker>
+  </gmap-marker>-->
      <!-- <gmap-info-window :position="{lat: laPharmacie.latlng.Rc ,lng: laPharmacie.latlng.Ac }"> 
         {{laPharmacie.nom}} 
-        </gmap-info-window>-->
+        </gmap-info-window>
 
 </div>
-</GmapMap>
+</GmapMap>-->
 
   </div>
 </template>
@@ -27,17 +28,32 @@
     data() {
        return {
         pharmacie: []
+        
     };
     },
   methods: {},
 
   created() {
+
     db.collection('pharmacie').get().then(snapshot => {
-    snapshot.forEach(doc => {
-      this.pharmacie.push(doc.data())
-    });
-})
-}}
+        snapshot.forEach(doc => {
+          
+          this.pharmacie.push(doc.data())
+      
+           //console.log('firebase',this.pharmacie)
+           /* console.log('local',JSON.parse(localStorage.getItem('pharmacieLocal')))
+            localStorage.setItem('pharmacieLocal', JSON.stringify(this.pharmacie))
+            this.pharmacie.push(JSON.parse(localStorage.getItem('pharmacieLocal')))*/
+          
+        });
+    })
+    
+    
+
+  }
+
+ 
+}
 
 
 </script>
